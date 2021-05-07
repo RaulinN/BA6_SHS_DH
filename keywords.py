c568bcd3c3f18dfd3801c390b82d4423c2199325
IraDH <ira.dobbelaere@epfl.ch>
2021-05-07 09:32:38 +0200

    New keywords

--- keywords.py
+++ keywords.py
@@ -1,7 +1,29 @@
 
 
-birth = "BIRTH"
-death = "DEATH"
-date = "DATE"
-location = "LOCATION"
-star = "MAIN SUSPECT"
+
+# CATEGORIES
+biography = "Données biographiques"
+formation = "Formation"
+functions = "Fonctions et mandats"
+
+# BIOGRAPHY
+name = "Nom :"
+first_name = "Prénom :"
+sex = "Sexe :"
+dates = "Dates :"
+birth = "Naissance: "
+death = "Décès :"
+birth_place = "Lieu naissance :"
+
+# FORMATION
+year = "année"
+title = "titre"
+category = "catégorie"
+place = "lieu"
+country = "pays"
+
+# FUNCTIONS
+duration = "durée"
+company = "entreprise/association éco"
+function = "fonction"
+company_body = "organe" 
